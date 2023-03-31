fc304871162b0421da220bb906ac6ba91d07be35
Divya Milind Kekade <kekadedivya@gmail.com>
2023-03-31 12:49:24 +0530

    conflict resolved

--- lib/screens/SingleGrievance.dart
+++ lib/screens/SingleGrievance.dart
@@ -156,7 +156,7 @@ class _SingleGrievanceState extends State<SingleGrievance> {
                           SizedBox(
                               width: MediaQuery.of(context).size.width * 1,
                               child: Text(
-                                "jdnfviuarjns jdhfiuqhneicmos bd73ed",
+                                "${widget.grievance['createrName']}",
                                 style: TextStyle(
                                     fontSize:
                                         MediaQuery.of(context).size.height *
