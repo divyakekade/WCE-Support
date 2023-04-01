df0fbebe2eb0128f263a4af123eaea108b2bc245
Divya Milind Kekade <kekadedivya@gmail.com>
2023-04-01 12:01:23 +0530

    grievances pages updated and feedback form

--- lib/screens/SingleUserPastGrievnaces.dart
+++ lib/screens/SingleUserPastGrievnaces.dart
@@ -21,18 +21,7 @@ class SingleUserPastGrievances extends StatefulWidget {
 class _SingleUserPastGrievancesState extends State<SingleUserPastGrievances> {
   bool _isInit = false;
   dynamic user ; 
-  List list = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten"
-  ];
+
   void initState(){
     user = Provider.of<Auth>(context,listen: false).user; 
 
@@ -59,110 +48,148 @@ class _SingleUserPastGrievancesState extends State<SingleUserPastGrievances> {
     List list = Provider.of<Griv>(context).sendGrievanceList();
     return Scaffold(
       appBar: Appbar(),
-      body: Column(
+      body: Stack(
         children: [
           Container(
-            margin: EdgeInsets.symmetric(
-                vertical: MediaQuery.of(context).size.height * 0.02),
-            padding: EdgeInsets.symmetric(
-                vertical: MediaQuery.of(context).size.height * 0.0052,
-                horizontal: MediaQuery.of(context).size.width * 0.05),
-            decoration: headingBoxDecoration,
-            child: Text(
-              'Your Past Grievances',
-              style: headingTextStyle,
-            ),
+            width: double.infinity,
+            height: double.infinity,
+            color: backgroundColor
           ),
-          Expanded(
-              // child: SingleChildScrollView(
-              // child: Container(
-              //     height: MediaQuery.of(context).size.height * 0.6,
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: list.length,
-                  itemBuilder: (context, index) {
-                    dynamic l = list[index];
-                    return (list[index]['userID']==user["_id"])?Container(
-                      margin: EdgeInsets.symmetric(
-                          vertical: MediaQuery.of(context).size.height * 0.01,
-                          horizontal: MediaQuery.of(context).size.width * 0.03),
-                      padding: EdgeInsets.symmetric(
-                          vertical: MediaQuery.of(context).size.height * 0.01,
-                          horizontal: MediaQuery.of(context).size.width * 0.03),
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 230, 230, 230),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromRGBO(159, 157, 157, 1),
-                            offset: Offset(2, 4),
-                            blurRadius: 4,
-                          )
-                        ],
-                      ),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 1,
-                              child: Text(
-                                "Subject of the grievance",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize:
-                                        MediaQuery.of(context).size.height *
+          Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.height * 0.02),
+              padding: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.height * 0.0052,
+                  horizontal: MediaQuery.of(context).size.width * 0.05),
+              decoration: headingBoxDecoration,
+              child: Text(
+                'Your Past Grievances',
+                style: headingTextStyle,
+              ),
+            ),
+            Expanded(
+                // child: SingleChildScrollView(
+                // child: Container(
+                //     height: MediaQuery.of(context).size.height * 0.6,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: list.length,
+                    itemBuilder: (context, index) {
+                      dynamic l = list[index];
+                      return (list[index]['userID']==user["_id"])
+                      ? Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical:
+                                      MediaQuery.of(context).size.height * 0.008,
+                                  horizontal:
+                                      MediaQuery.of(context).size.width * 0.035),
+                              padding: EdgeInsets.only(
+                                  left: MediaQuery.of(context).size.width * 0.02,
+                                  top: MediaQuery.of(context).size.height * 0.01,
+                                  bottom: MediaQuery.of(context).size.height * 0.01),
+                              decoration: BoxDecoration(
+                                color: cardColor,
+                                boxShadow: const [
+                                  BoxShadow(
+                                  color: Color.fromRGBO(194, 194, 194, 1),
+                                    offset: Offset(2, 4),
+                                    blurRadius: 5,
+                                  )
+                                ],
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Column(children: [
+                                        SizedBox(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.78,
+                                            child: Text(
+                                              list[index]['subject'],
+                                              style: TextStyle(
+                                                color: const Color.fromARGB(255, 15, 100, 112),
+                                                  fontWeight: FontWeight.w500,
+                                                  fontSize: MediaQuery.of(context)
+                                                          .size
+                                                          .height *
+                                                      0.02),
+                                            )),
+                                        SizedBox(
+                                            height: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.02),
+                                        SizedBox(
+                                          width:
+                                              MediaQuery.of(context).size.width *
+                                                  0.78,
+                                          child: Text(
+                                            list[index]['description'],
+                                            maxLines: 2,
+                                            style: TextStyle(
+                                                overflow: TextOverflow.ellipsis,
+                                                fontSize: MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.018),
+                                          ),
+                                        ),
+                                      ]),
+                                      SizedBox(
+                                        width: MediaQuery.of(context).size.width *
+                                            0.022,
+                                      ),
+                                      Container(
+                                        padding: const EdgeInsets.all(0.0),
+                                        decoration: BoxDecoration(
+                                            color: imagebutton,
+                                            borderRadius:
+                                                BorderRadius.circular(50)),
+                                        child: GestureDetector(
+                                            onTap: () {
+                                              Navigator.of(context).push(MaterialPageRoute(
+                                                              builder: (context) =>
+                                                                  SingleGrievance(grievance:list[index])));
+                                            },
+                                            child: const Icon(
+                                              Icons.chevron_right,
+                                              size: 32,
+                                              color: headingColor,
+                                            )),
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(
+                                      height: MediaQuery.of(context).size.width *
+                                          0.03),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                        right: MediaQuery.of(context).size.width *
                                             0.02),
-                              )),
-                          SizedBox(
-                              height: MediaQuery.of(context).size.width * 0.03),
-                          Text(
-                            list[index]['subject'],
-                            style: TextStyle(
-                                overflow: TextOverflow.ellipsis,
-                                fontSize:
-                                    MediaQuery.of(context).size.height * 0.017),
-                          ),
-                          SizedBox(
-                              height: MediaQuery.of(context).size.width * 0.03),
-                          Row(
-                            children: [
-                              Text(
-                                "Status: ",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize:
-                                        MediaQuery.of(context).size.height *
-                                            0.018),
+                                    width: MediaQuery.of(context).size.width * 1,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text("01/04/2023",style: TextStyle(color: headingColor,fontSize: MediaQuery.of(context).size.height*0.014),),
+                                        Text("9:30 AM",style: TextStyle(color: headingColor,fontSize: MediaQuery.of(context).size.height*0.014),)
+                                      ],
+                                    ),
+                                  )
+                                ],
                               ),
-                              Text(
-                                list[index]['status'],
-                                style: TextStyle(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height *
-                                            0.017),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              ElevatedButton(
-                                  onPressed: ()=>{
-                                          Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    SingleGrievance(
-                                                        grievance:
-                                                            list[index])))
-                                  },
-                                  style: buttonStyle,
-                                  child: const Text("View Details"))
-                            ],
-                          )
-                        ],
-                      ),
-                    ):Container();
-                  }))
-          // ),
-        ],
+                            )
+                      :Container();
+                    }))
+            // ),
+          ],
+        ),]
       ),
     );
   }
