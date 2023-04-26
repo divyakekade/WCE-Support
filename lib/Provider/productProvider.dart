5b76179b3e8ebdc188e888fe7f9150fc428516aa
Sahil Pravin Thakur <85469451+sahilthakur007@users.noreply.github.com>
2023-04-26 21:16:49 +0530

    multiple-user

--- lib/Provider/productProvider.dart
+++ lib/Provider/productProvider.dart
@@ -6,7 +6,7 @@ import '../Exceptions/httpexception.dart';
 import 'package:http/http.dart' as http;
 
 class Prod with ChangeNotifier {
-  static const ip = "192.168.43.89";
+  static const ip = "192.168.43.95";
   static const url1="";
   static const url2="";
   var products = [];
--- lib/screens/CreateUser.dart
+++ lib/screens/CreateUser.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
 import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +10,11 @@ import 'package:wce_support/screens/HomeScreen.dart';
 import 'package:wce_support/screens/SideMenuNavigation.dart';
 import 'package:wce_support/widgets/CustomSnackbar.dart';
 import 'package:wce_support/widgets/errorDialogBox.dart';
+import 'package:file_picker/file_picker.dart';
+// import 'package:flutter_excel/excel.dart';
+import 'dart:convert';
+import 'package:csv/csv.dart';
+import 'package:excel/excel.dart' hide Border;
 
 class CreateUser extends StatefulWidget {
   const CreateUser({super.key});
@@ -49,11 +57,60 @@ class _CreateUserState extends State<CreateUser> {
   ];
 
   List<String> rolesList = <String>["select role", "Student", "Management"];
+  Future<void> pickfile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result == null) {
+      return;
+    }
+    print(result.files.first.name);
+    var filePath = result.files.first.path!;
+    final input = File(filePath).openRead();
+    final fields = await input
+        .transform(utf8.decoder)
+        .transform(const CsvToListConverter())
+        .toList();
+    print(fields.length);
+    for (int i = 1; i < fields.length; i++) {
+      String firstName = fields[i][0];
+      String lastName = fields[i][1];
+      String userName = fields[i][2];
+      String email = fields[i][3];
+      String mobileNo = fields[i][4].toString();
+      String password = fields[i][5].toString();
+      String role = fields[i][6];
+      String department = fields[i][7];
+      String year = fields[i][8];
+            print(firstName);
+      final user = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "username": userName,
+        "email": email,
+        "password": password,
+        "role": role,
+        "department": department,
+        "year": role == "Student" ? year : "other",
+        "mobileNo": mobileNo
+      };
+      try {
+        await Provider.of<Auth>(context, listen: false).createUser(user);
+        
+        // Navigator.of(context).push(MaterialPageRoute(
+        //     builder: (context) =>
+        //         SideMenuNavigation(loadedPage: 'create_user')));
+      } catch (error) {
+        showErrorDialogBox2(error.toString(), context);
+      }
+    }
+    // User selected an Excel sheet, parse the data
+showCustomSnackbar(1, "All user created successfully!", context);
+    // Send the data to the backend
+  }
 
   Future<void> createUser() async {
     final user = {
       "firstName": firstName,
-       "lastName": lastName,
+      "lastName": lastName,
       "username": username,
       "email": email,
       "password": password,
@@ -65,15 +122,16 @@ class _CreateUserState extends State<CreateUser> {
     try {
       await Provider.of<Auth>(context, listen: false).createUser(user);
       showCustomSnackbar(1, "New user created successfully!", context);
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => SideMenuNavigation(loadedPage: 'create_user')));
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => SideMenuNavigation(loadedPage: 'create_user')));
     } catch (error) {
       showErrorDialogBox2(error.toString(), context);
     }
   }
+
   Future<bool> backNavigation() async {
-      Navigator.of(context).popAndPushNamed(HomeScreen.routeUrl);
-       return false;
+    Navigator.of(context).popAndPushNamed(HomeScreen.routeUrl);
+    return false;
   }
 
   @override
@@ -82,13 +140,12 @@ class _CreateUserState extends State<CreateUser> {
       // resizeToAvoidBottomInset: false,
       body: WillPopScope(
         onWillPop: backNavigation,
-        child: Stack(
-          children: [
-             Container(
+        child: Stack(children: [
+          Container(
               width: double.infinity,
               height: double.infinity,
               color: backgroundColor),
-            Column(
+          Column(
             children: [
               Container(
                 margin: EdgeInsets.symmetric(
@@ -127,7 +184,8 @@ class _CreateUserState extends State<CreateUser> {
                                 style: TextStyle(
                                     color: headingColor,
                                     fontSize:
-                                        MediaQuery.of(context).size.height * 0.018),
+                                        MediaQuery.of(context).size.height *
+                                            0.018),
                               ),
                             ],
                           ),
@@ -141,52 +199,52 @@ class _CreateUserState extends State<CreateUser> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width*0.4,
-                              child: TextFormField(
-                              onChanged: (name) {
-                                setState(() {
-                                  firstName = name;
-                                });
-                              },
-                              decoration: const InputDecoration(
-                                  labelText: 'first name',
-                                  contentPadding: EdgeInsets.all(12),
-                                  border: OutlineInputBorder(
-                                      // borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                                      borderSide: BorderSide(
-                                          color: Colors.black, width: 2.0)),
-                                  focusedBorder: OutlineInputBorder(
-                                      // borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                                      borderSide: BorderSide(
-                                    color: Colors.black,
-                                  ))),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.4,
+                                child: TextFormField(
+                                  onChanged: (name) {
+                                    setState(() {
+                                      firstName = name;
+                                    });
+                                  },
+                                  decoration: const InputDecoration(
+                                      labelText: 'first name',
+                                      contentPadding: EdgeInsets.all(12),
+                                      border: OutlineInputBorder(
+                                          // borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                          borderSide: BorderSide(
+                                              color: Colors.black, width: 2.0)),
+                                      focusedBorder: OutlineInputBorder(
+                                          // borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                          borderSide: BorderSide(
+                                        color: Colors.black,
+                                      ))),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.4,
+                                child: TextFormField(
+                                  onChanged: (name) {
+                                    setState(() {
+                                      lastName = name;
+                                    });
+                                  },
+                                  decoration: const InputDecoration(
+                                      labelText: 'last name',
+                                      contentPadding: EdgeInsets.all(12),
+                                      border: OutlineInputBorder(
+                                          // borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                          borderSide: BorderSide(
+                                              color: Colors.black, width: 2.0)),
+                                      focusedBorder: OutlineInputBorder(
+                                          // borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                          borderSide: BorderSide(
+                                        color: Colors.black,
+                                      ))),
+                                ),
+                              ),
+                            ],
                           ),
-                            ),
-                          Container(
-                            width: MediaQuery.of(context).size.width*0.4,
-                            child: TextFormField(
-                              onChanged: (name) {
-                                setState(() {
-                                  lastName = name;
-                                });
-                              },
-                              decoration: const InputDecoration(
-                                  labelText: 'last name',
-                                  contentPadding: EdgeInsets.all(12),
-                                  border: OutlineInputBorder(
-                                      // borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                                      borderSide: BorderSide(
-                                          color: Colors.black, width: 2.0)),
-                                  focusedBorder: OutlineInputBorder(
-                                      // borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                                      borderSide: BorderSide(
-                                    color: Colors.black,
-                                  ))),
-                            ),
-                          ),
-                          ],),
-                          
                           SizedBox(
                             height: MediaQuery.of(context).size.height * 0.025,
                           ),
@@ -390,8 +448,8 @@ class _CreateUserState extends State<CreateUser> {
                               : const SizedBox(),
                           showYearBranch
                               ? SizedBox(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.025)
+                                  height: MediaQuery.of(context).size.height *
+                                      0.025)
                               : const SizedBox(),
                           ElevatedButton(
                             onPressed: createUser,
@@ -399,10 +457,20 @@ class _CreateUserState extends State<CreateUser> {
                             child: Text(
                               "Submit",
                               style: TextStyle(
-                                  fontSize:
-                                      MediaQuery.of(context).size.height * 0.025),
+                                  fontSize: MediaQuery.of(context).size.height *
+                                      0.025),
                             ),
-                          )
+                          ),
+                          ElevatedButton(
+                            onPressed: pickfile,
+                            style: buttonStyle,
+                            child: Text(
+                              "Upload user ",
+                              style: TextStyle(
+                                  fontSize: MediaQuery.of(context).size.height *
+                                      0.025),
+                            ),
+                          ),
                         ],
                       ),
                     ),
